94e0246ffbf680bbdd2359a873b9265b4d65a690
gontijoVitor <vitorgontijo2017@gmail.com>
2025-02-04 14:21:50 -0300

    clean repo

--- src/assets/imports.js
+++
@@ -1,7 +0,0 @@
-import bootstrapCss from './bootstrap-5.3.3/css/bootstrap.css';
-import bootstrapJs from'./bootstrap-5.3.3/js/bootstrap.bundle.min.js';
-import fontawesome from './fontawesome-6.6.0/css/all.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom'
-
